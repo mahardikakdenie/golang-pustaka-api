748a792e2ba3bd290c502cb9c6b95efd5f6d7104
MahardikaKDenie <dikamahar884@gmail.com>
2022-04-17 00:50:22 +0700

    Implement .env

--- main.go
+++ main.go
@@ -2,18 +2,33 @@ package main
 
 import (
 	"log"
+	"os"
 	entity "pustaka-api/entity"
 	"pustaka-api/routes"
 
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
 
+func goDotEnvVariable(key string) string {
+
+	// load .env file
+	err := godotenv.Load(".env")
+
+	if err != nil {
+		log.Fatalf("Error loading .env file")
+	}
+
+	return os.Getenv(key)
+}
+
 func main() {
+	DB_NAME := goDotEnvVariable("DB_NAME")
 	router := gin.Default()
 	// refer https://github.com/go-sql-driver/mysql#dsn-data-source-name for details
-	dsn := "root:@tcp(localhost:3306)/pustaka-api?charset=utf8mb4&parseTime=True&loc=Local"
+	dsn := "root:@tcp(localhost:3306)/" + DB_NAME + "?charset=utf8mb4&parseTime=True&loc=Local"
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
 		log.Fatal("DB connection error: ", err)
